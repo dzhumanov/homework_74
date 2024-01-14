d8640774734c868f3695818b1e3ea28901468f41
Temirlan <temadjumanov@gmail.com>
2024-01-15 01:21:15 +0600

    Completed task

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import express from "express";
+import messagesRouter from "./routers/messages";
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use("/messages", messagesRouter);
 
 const run = async () => {
-    app.listen(port, () => {
-      console.log(`Server started on ${port} port!`);
-    });
-  };
-  
-  void run();
-  
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+};
+
+void run();
--- routers/messages.ts
+++ routers/messages.ts
@@ -1,8 +1,47 @@
-import { Router } from "express";
-const messagesRouter = Router();
+import express, { Router } from "express";
+import { promises as fs } from "fs";
+import { Message } from "../types";
 
-messagesRouter.get('/messages', async (req, res) => {
-    res.send('Messages will be here');
-})
+const messagesRouter: Router = express.Router();
+const messagesPath = "./messages";
 
-messagesRouter.post('/')
+messagesRouter.post("/create", async (req, res) => {
+  const messageItem: Message = {
+    message: req.body.message,
+    datetime: new Date().toISOString().replace(/:/g, "_"),
+  };
+  const fileName = `${messagesPath}/${messageItem.datetime}.txt`;
+
+  try {
+    await fs.writeFile(fileName, JSON.stringify(messageItem.message));
+    res.send(messageItem);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+messagesRouter.get("/", async (req, res) => {
+  try {
+    const files = await fs.readdir(messagesPath);
+
+    const promises = files.map(async (file) => {
+      const fileContent = await fs.readFile(`${messagesPath}/${file}`);
+      const data: string = fileContent.toString();
+
+      const oneMessage: Message = {
+        message: JSON.parse(data),
+        datetime: file,
+      };
+
+      return oneMessage;
+    });
+
+    const messages = await Promise.all(promises);
+    const lastMessages = messages.slice(-5);
+    res.send(lastMessages);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export default messagesRouter;
